2825375d4b2a0fab8270bf1e05b389b30ccd6bed
gustavoxp6 <123974890+gustavoxp6@users.noreply.github.com>
2023-11-12 16:14:12 -0300

    metodo buscar

--- projeto_senai/src/dao/FLUXODECAIXADAO.java
+++ projeto_senai/src/dao/FLUXODECAIXADAO.java
@@ -60,4 +60,33 @@ public class FLUXODECAIXADAO {
             System.out.println("Erro ao consultar registro!\n" + erro_consultar_Registro.getMessage());
         }
     }
+    public void Buscar_fluxo(FLUXODECAIXA fluxodecaixa){
+        String sql = "SELECT * from fluxo_de_caixa WHERE cod = "+fluxodecaixa.getCod();
+        try {
+            java.sql.Statement stmt = conectar.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                fluxodecaixa.setDinheiro(rs.getDouble("dinheiro"));
+                fluxodecaixa.setCartao(rs.getDouble("cartao"));
+                fluxodecaixa.setContas(rs.getDouble("contas"));
+                fluxodecaixa.setData(rs.getString("data"));
+                fluxodecaixa.setDespesa_bancaria(rs.getDouble("despesa_bancaria"));
+                fluxodecaixa.setFornecedores(rs.getDouble("fornecedores"));
+                fluxodecaixa.setImpostos(rs.getDouble("impostos"));
+                fluxodecaixa.setManuntencoes(rs.getDouble("manutencao"));
+                fluxodecaixa.setOutras_saidas(rs.getDouble("outras_saidas"));
+                fluxodecaixa.setOutros_entradas(rs.getDouble("outras_entradas"));
+                fluxodecaixa.setParcela_equipamento(rs.getDouble("parcela_de_equipamento"));
+                fluxodecaixa.setProlabore(rs.getDouble("prolabore"));
+                fluxodecaixa.setSalario_funcionario(rs.getDouble("salario_funcionario"));
+                fluxodecaixa.setSaldo_anterior(rs.getDouble("saldo_dia_anterior"));
+                fluxodecaixa.setTotal_saidas(rs.getDouble("total_de_saidas"));
+                fluxodecaixa.setTotal_entrada(rs.getDouble("total_de_entradas"));
+                fluxodecaixa.setSaldo_do_dia(rs.getDouble("saldo_do_dia"));
+                fluxodecaixa.setSaldo_atual(rs.getDouble("saldo_atual"));
+            }
+    }catch(SQLException erro){
+        JOptionPane.showMessageDialog(null, "Erro ao buscar os dados");
+        }
+    }
 }
